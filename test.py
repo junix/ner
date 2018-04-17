2ac085ffd82c6f80112eff2af3c197729d3a760a
wanglijun <juns1984@qq.com>
2018-04-17 14:24:16 +0800

    update model

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
+from flask import Blueprint
+from flask_restplus import Api
+
+# api = Api(Blueprint)
+
+
+
 from flask_restplus import Api
 
 from api.ner.ner import ns
-api = Api(
+
+blueprint = Blueprint('api', __name__)
+
+api = Api(blueprint,
     title='My Title',
     version='1.0',
     description='A description',
@@ -13,6 +23,7 @@ api.add_namespace(ns)
 from flask import Flask
 
 app = Flask(__name__)
-api.init_app(app)
+# api.init_app(app)
+app.register_blueprint(blueprint)
 
 app.run(debug=True)
