fd33535fbb691a73849ae3fbf41e922eb0d0ea14
wanglijun <juns1984@qq.com>
2018-04-17 21:44:24 +0800

    update model

--- api/ner/serializers.py
+++ api/ner/serializers.py
@@ -2,9 +2,9 @@ from flask_restplus import fields
 from api.restplus import api
 
 ner_result = api.model('entity recognize result', {
-    'entity': fields.String(description='entity')
+    'entity': fields.String(description='recognized entity')
 })
 
 ner_request = api.model('entity recognize request', {
-    'query': fields.String(description='query')
+    'query': fields.String(description='query content')
 })
