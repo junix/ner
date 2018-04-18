a99779afc2d9544151fa9a859157cbe0ca8fa5ec
wanglijun <juns1984@qq.com>
2018-04-18 15:17:45 +0800

    add grpc call

--- dataset/transformer.py
+++ dataset/transformer.py
@@ -1,8 +1,11 @@
 import numpy as np
+# import time
 
 from word2vec import wv
 
 
 def transform(words):
+    # beg = time.time()
     vecs = wv.get_batch(words)
+    # print(len(words), '->', time.time() - beg)
     return np.array(vecs)
