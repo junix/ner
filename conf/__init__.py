4a394d14671c5919db09b37f880ab05c1db7916f
wanglijun <juns1984@qq.com>
2018-04-18 16:08:01 +0800

    add grpc call

--- conf/__init__.py
+++ conf/__init__.py
@@ -3,4 +3,4 @@
 """
 GENSIM_MODEL_PATH = "/Users/junix/wordvec_models/400d/400d.model.bin"
 WORD2VEC_RPC_SERVER_HOST = 'localhost'
-WORD2VEC_RPC_SERVER_PORT = '8080'
+WORD2VEC_RPC_SERVER_PORT = '9191'
