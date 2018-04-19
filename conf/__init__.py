d82c207b4f7c23d0b3c2cf825b20774d66747ae5
wanglijun <juns1984@qq.com>
2018-04-19 19:32:25 +0800

    支持多个 server 的容错处理

--- conf/__init__.py
+++ conf/__init__.py
@@ -2,8 +2,6 @@
 基本的配置
 """
 GENSIM_MODEL_PATH = "/Users/junix/wordvec_models/400d/400d.model.bin"
-WORD2VEC_RPC_SERVER_HOST = 'localhost'
-WORD2VEC_RPC_SERVER_PORT = '9191'
 
 word2vec_rpc_servers = (
     ('localhost', 9191),
