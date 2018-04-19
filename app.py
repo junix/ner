f8b50e6e530719c2166d2e21207f396bf75c0dcf
wanglijun <juns1984@qq.com>
2018-04-19 16:51:00 +0800

    支持多个 server 的容错处理

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from __init__ import create_app
-from api.ner.business import get_entity
 
 app = create_app()
 
@@ -7,5 +6,4 @@ app = create_app()
 application = app
 
 if __name__ == '__main__':
-    # get_entity("s")
     app.run(debug=app.config['FLASK_DEBUG'], host='0.0.0.0', port=8888)
