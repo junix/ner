844d166caaa562fd9bb0defce58bdfeee12cdb75
wanglijun <juns1984@qq.com>
2018-04-26 21:55:29 +0800

    use port 28080

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = create_app()
 application = app
 
 if __name__ == '__main__':
-    app.run(debug=app.config['FLASK_DEBUG'], host='0.0.0.0', port=8888)
+    app.run(debug=app.config['FLASK_DEBUG'], host='0.0.0.0', port=28080)
