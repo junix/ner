c8351c40e88a7b86f07975aace4ad071b2e62fb0
wanglijun <juns1984@qq.com>
2018-04-28 20:48:36 +0800

    update dataset

--- conf/__init__.py
+++ conf/__init__.py
@@ -1,6 +1,11 @@
+import torch
+
 """
 基本的配置
 """
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 GENSIM_MODEL_PATH = "/opt/word2vec_models/200/model.bin"
 
 word2vec_rpc_servers = (
