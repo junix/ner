ee014cadf2f943a6a3265c6b7308ec68d06ec717
wanglijun <juns1984@qq.com>
2018-05-02 21:14:03 +0800

    device 设置为全局变量

--- dataset/gen_dataset.py
+++ dataset/gen_dataset.py
@@ -208,7 +208,7 @@ def _read_words():
                 pass
 
 
-_all_words = tuple(set(_read_words()))
+_all_words = [w for w in set(_read_words()) if len(w) <= 4]
 _all_words_and_puncts = _all_words + puncts
 
 
