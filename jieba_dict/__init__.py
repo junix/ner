cd9478653bcb917c94a492cfb784a3bea7ec0bca
wanglijun <juns1984@qq.com>
2018-05-10 13:52:44 +0800

    device 设置为全局变量

--- jieba_dict/__init__.py
+++ jieba_dict/__init__.py
@@ -2,8 +2,13 @@ import os
 import jieba
 
 JIEBA_USER_DICT = os.path.dirname(__file__) + '/dict.dat'
-jieba.load_userdict(JIEBA_USER_DICT)
+
+_loaded_dict = False
 
 
 def init_user_dict():
-    pass
+    global _loaded_dict
+    if _loaded_dict:
+        return
+    jieba.load_userdict(JIEBA_USER_DICT)
+    _loaded_dict = True
