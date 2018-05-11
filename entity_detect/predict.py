de35681a654b5e2614cd1a574e9a00159579d6a7
wanglijun <juns1984@qq.com>
2018-05-11 10:30:37 +0800

    fix

--- entity_detect/predict.py
+++ entity_detect/predict.py
@@ -50,9 +50,7 @@ def load_predict(output_keyword=False):
                 old_keywords = keywords
 
         if not old_keywords:
-            words = list(jieba.cut(sentence))
-            words = clean_phrase(words)
-            old_keywords = ''.join(words)
+            old_keywords = remove_stopwords(sentence)
         return old_category, old_keywords
 
     return predict
@@ -70,15 +68,23 @@ def select_keywords(words, tags):
                 category.append(' ')
             category.append(word)
         prev_tag = tag
-    keywords = clean_phrase(keywords)
+    keywords = remove_stopwords(keywords)
     return ''.join(category), ''.join(keywords)
 
 
-def clean_phrase(words):
+def remove_stopwords(words):
+    import jieba
     from jieba_dict import is_stopword
     from itertools import dropwhile
-    words.reverse()
-    words = list(dropwhile(lambda x: is_stopword(x), words))
-    words.reverse()
-    words = list(dropwhile(lambda x: is_stopword(x), words))
-    return words
+    if isinstance(words, (list, tuple)):
+        words.reverse()
+        words = list(dropwhile(lambda x: is_stopword(x), words))
+        words.reverse()
+        words = list(dropwhile(lambda x: is_stopword(x), words))
+        return words
+    elif isinstance(words, str):
+        words = list(jieba.cut(words))
+        words = remove_stopwords(words)
+        return ''.join(words)
+
+    raise ValueError("{} can't remove stopwords".format(words))
