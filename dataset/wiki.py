631201b2a22401cb8ca4912490814463527696b3
wanglijun <juns1984@qq.com>
2018-05-26 18:50:21 +0800

    remove gensim/rpc functions

--- dataset/wiki.py
+++ dataset/wiki.py
@@ -1,10 +1,18 @@
 from conf import WIKI_DATASET
 from regularize import regularize_punct
+from utils import join_words2
 
 
 def generate_sentences():
     for line in open(WIKI_DATASET):
         yield from generate_sentence(line)
+
+
+def _rejoin(sentence):
+    if not sentence:
+        return sentence
+    words = sentence.split(' ')
+    return join_words2(words)
 
 
 def generate_sentence(text):
@@ -13,6 +21,6 @@ def generate_sentence(text):
         if c in '，、。（）':
             sentence, acc = regularize_punct(''.join(acc)), []
             if len(sentence) > 4:
-                yield sentence
+                yield _rejoin(sentence)
         else:
             acc.append(c)
